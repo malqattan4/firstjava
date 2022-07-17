d09ca9af28f399117018218462cad0fd23b5733a
malqattan4 <106008076+malqattan4@users.noreply.github.com>
2022-07-17 13:33:18 +0300

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+	public static void main(String[] args) {
+		System.out.println("My name is Maryam Alqattan");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Riyadh, SA");
+	}
+}
